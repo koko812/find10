d2a34d51c69f588381646aaf0d5ad67f86621dbd
koko812 <iwakawa.koichi.q5@dc.tohoku.ac.jp>
2025-05-10 16:21:04 +0900

    disp_nums

--- main.js
+++ main.js
@@ -8,9 +8,9 @@ let container = null
 let svgTemplate = null;
 
 // まず角度を変える
-class Circle{
-    constructor(){
-        let angle = Math.random() * 180
+class Circle {
+    constructor(combination) {
+        let angle = Math.random() * 0
         const element = document.createElement('div')
         this.element = element
         // this をつけるとめんどくさいので，コンストラクタ内に限ってはつけなくてもいいか
@@ -25,7 +25,27 @@ class Circle{
         console.log('create Circle');
         element.append(svg)
 
-        const num = 
+        angle += 180
+        const numSize = 25
+        for (let i = 0; i < 3; i++) {
+            const num = document.createElement('div')
+            element.append(num)
+            num.style.position = 'absolute'
+            num.style.width = `${numSize}px`
+            num.style.height = `${numSize}px`
+            console.log(Math.cos(angle), Math.sin(angle));
+            const x = 50 + 25 * Math.cos(angle / 180 * Math.PI) - numSize / 2
+            const y = 50 + 25 * Math.sin(angle / 180 * Math.PI) - numSize / 2
+            num.style.top = `${y}px`
+            num.style.left = `${x}px`
+            // text 扱う時は，display あたりは指定する，という脳みそになっておこう
+            num.style.display = 'flex'
+            num.style.alignItems = 'center'
+            num.style.justifyContent = 'center'
+            num.textContent = combination[i]
+            num.style.fontSize = `${numSize*0.8}px`
+            angle += 120
+        }
     }
 }
 // 次は，このノードをいっぱい出して，その中に数字をつけるところだね
@@ -48,6 +68,7 @@ const init = () => {
 
 window.onload = () => {
     init()
-    const circle = new Circle()
+    // ここ，textContent には数字もそのまま入れてくれるらしくて便利だね
+    const circle = new Circle([1, 2, 3])
     container.append(circle.element)
 }
