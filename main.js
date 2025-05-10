e303f9875f9b7af9e5258f72845b772f3ac62690
koko812 <iwakawa.koichi.q5@dc.tohoku.ac.jp>
2025-05-10 16:00:27 +0900

    disp_circle

---
+++ main.js
@@ -0,0 +1,42 @@
+// index の方で，display = none にしてるのに，こっちに持ってきたら見えるのが謎？？
+// とりあえず html のなかで雛形作って．こっちに持ってきて使うという思想は理解できるんだけど
+
+const width = 300
+const height = 300
+
+let container = null
+let svgTemplate = null;
+
+class Circle{
+    constructor(){
+        const element = document.createElement('div')
+        this.element = element
+        this.element.style.position = 'absolute'
+        // cloneNode で true にしないと，タグの中身をもらえない
+        // ただ，true にすると，id 被りなどが発生しうるから注意とのことだ
+        const svg = svgTemplate.cloneNode(true)
+        console.log('create Circle');
+        this.element.append(svg)
+    }
+
+}
+const init = () => {
+    svgTemplate = document.getElementsByTagName('svg')[0]
+    // 親ノードと切り離すということをやってるらしい（謎）
+    svgTemplate.remove()
+    svgTemplate.style.display = "block"
+
+    container = document.createElement('div')
+    container.style.position = 'relative'
+    container.style.width = `${width}px`
+    container.style.height = `${height}px`
+    container.style.border = 'solid 1px #000'
+    container.style.overflow = 'hidden'
+    document.body.append(container)
+}
+
+window.onload = () => {
+    init()
+    const circle = new Circle()
+    container.append(circle.element)
+}
